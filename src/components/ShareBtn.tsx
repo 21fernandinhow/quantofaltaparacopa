9e089ab48e02b877cad287fcb27b492cd057d692
Fernando Carvalho <21fernandinhow@gmail.com>
2025-09-10 16:08:33 -0300

    update share links

--- src/components/ShareBtn.tsx
+++ src/components/ShareBtn.tsx
@@ -1,23 +1,53 @@
+"use client"
+
+import { useState } from "react";
 import Link from "next/link";
 import { HiShare } from "react-icons/hi";
+import { FaWhatsapp, FaXTwitter } from "react-icons/fa6";
 
 interface ShareBtnProps {
     days: number | string
 }
 
-const ShareBtn = ({days}: ShareBtnProps) => {
-    const message = `Faltam ${days} dias para a Copa do Mundo! Acompanhe a contagem regressiva em: https://quantofaltaparacopa.com.br/`
+const ShareBtn = ({ days }: ShareBtnProps) => {
+    const [open, setOpen] = useState(false);
+
+    const postUrl = "https://quantofaltaparacopa.com.br/";
+    const shareMessage = `Faltam ${days} dias para a Copa do Mundo! Acompanhe a contagem regressiva em: ${postUrl}`;
 
     return (
-        <Link 
-            id="shareBtn" 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            href={`https://wa.me/?text=${encodeURIComponent(message)}`}
-        >
-            <HiShare />
-        </Link>
-    )
-}
+        <>
+            <button
+                id="shareBtn"
+                onClick={() => setOpen((prev) => !prev)}
+            >
+                <HiShare />
+            </button>
 
-export default ShareBtn
+            <div className="share-container">
+                {open && (
+                    <div className="share-menu">
+                        <Link
+                            href={`https://wa.me/?text=${encodeURIComponent(shareMessage)}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaWhatsapp />
+                        </Link>
+
+                        <Link
+                            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(shareMessage)}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaXTwitter />
+                        </Link>
+                    </div>
+                )}
+            </div>
+        </>
+
+    );
+};
+
+export default ShareBtn;
