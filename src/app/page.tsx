9a5d79ae670bc968b1be0d4069d38b89e339852a
Fernando Carvalho <21fernandinhow@gmail.com>
2025-10-04 21:40:06 -0300

    fix get days to world cup hours function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,13 @@ import Script from "next/script";
 export const dynamic = "force-dynamic";
 
 const getDaysToWorldCup = () => {
-  return Math.ceil(
-    (new Date(Date.UTC(2026, 5, 11)).getTime() - new Date().getTime()) /
-    (1000 * 60 * 60 * 24)
-  );
+  const today = new Date();
+  const target = new Date(2026, 5, 11);
+  
+  target.setHours(0, 0, 0, 0);
+  today.setHours(0, 0, 0, 0);
+
+  return Math.ceil((target.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
 };
 
 export const generateMetadata = async (): Promise<Metadata> => {
