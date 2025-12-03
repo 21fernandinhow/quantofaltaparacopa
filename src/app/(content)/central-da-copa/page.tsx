c2fb010bfd595bd126286ad0d48aecef4efec04e
Fernando Carvalho <21fernandinhow@gmail.com>
2025-12-03 17:14:33 -0300

    world cup central route

---
+++ src/app/(content)/central-da-copa/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const generateMetadata = (): Metadata => ({
+    title: "Central da Copa | Quanto Falta Para a Copa",
+});
+
+const WorldCupCentral = () => (
+    <>
+        <h1>Em breve</h1>
+    </>
+)
+
+
+export default WorldCupCentral
