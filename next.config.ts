d07e1be16478b88b9bc4885c3bc698d3cb353553
Fernando Carvalho <21fernandinhow@gmail.com>
2025-12-05 22:35:28 -0300

    FODASE

--- next.config.ts
+++ next.config.ts
@@ -4,33 +4,6 @@ const nextConfig: NextConfig = {
   sassOptions: {
     sourceMap: true,
   },
-
-  webpack(config) {
-    const rules = config.module?.rules as unknown[];
-
-    rules.forEach((rule) => {
-      if (
-        typeof rule === "object" &&
-        rule !== null &&
-        "oneOf" in rule &&
-        Array.isArray((rule as any).oneOf)
-      ) {
-        (rule as any).oneOf.forEach((one: any) => {
-          if (Array.isArray(one.use)) {
-            one.use = one.use.filter(
-              (u: any) =>
-                !(
-                  typeof u === "object" &&
-                  u?.loader?.includes("resolve-url-loader")
-                )
-            );
-          }
-        });
-      }
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
