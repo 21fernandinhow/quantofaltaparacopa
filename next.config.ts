02cf2f0210dee3a5feed026d6d0a767eec6e8cc9
Fernando Carvalho <21fernandinhow@gmail.com>
2025-12-05 22:13:35 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    sourceMap: true,
+  },
 };
 
 export default nextConfig;
