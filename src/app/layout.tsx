db47c2856151771ce393c514bcaa69acec9d1f4e
Fernando Carvalho <21fernandinhow@gmail.com>
2026-03-14 12:27:10 -0300

    add adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/index.scss";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -8,6 +9,11 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body>
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2366343909610236"
+          strategy="afterInteractive"
+          crossOrigin="anonymous"
+        />
         {children}
       </body>
     </html>
